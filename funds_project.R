cc2c1f831cdde9b376c7e5b5c0d21d278493ab29
uguraskar <uguraskar@gmail.com>
2020-12-26 20:02:59 +0300

    Update funds_project.R

--- funds_project.R
+++ funds_project.R
@@ -48,6 +48,25 @@ df_plot_categories = df_plot_price %>%
   group_by(category, fund_type) %>%
   summarize(avg_daily_change=mean(avg_daily_change,na.rm=TRUE),stdev=mean(stdev,na.rm = TRUE), annual_change=mean(annual_change,na.rm=TRUE))
 
+df_today=df_clean %>% 
+  filter(date==ymd("2020-11-16")) %>%
+  mutate(total_value_millions=round(total_value/1000000,2), total_value_bins=floor(total_value/10000000))
+
+df_today_investors= df_clean %>% 
+  filter(date==ymd("2020-11-16"))
+
+df_valued_funds = df_clean %>%
+  filter(date == ymd("2019-11-18") | date == ymd("2020-11-16")) %>%
+  select(date,fund_type, category, code, company_name ,name, total_value, price) %>%
+  arrange(code, name, date) %>%
+  group_by(code, name) %>%
+  mutate(previous_price=as.numeric(lag(price,n=1)), 
+         change_price_percentage=100*(price-as.numeric(lag(price,n=1)))/ as.numeric(lag(price,n=1)),
+         previous_total_value = as.numeric(lag(total_value,n=1)),
+         change_total_value_percentage=100*(total_value-as.numeric(lag(total_value,n=1)))/ as.numeric(lag(total_value,n=1))) %>%
+  filter(date==ymd('2020-11-16')) %>%
+  select(date,fund_type, category, code, name, price, previous_price, change_price_percentage, total_value, previous_total_value,change_total_value_percentage)
+
 setwd(dirname(rstudioapi::getSourceEditorContext()$path)) #Sets the current working directory.
 df_clean = readRDS('tefas_df_clean.rds')
 
@@ -55,8 +74,6 @@ categories =
   df_clean %>% 
   distinct(category) %>% 
   unlist(.,use.names = FALSE)
-
-print(categories)
 
 # header board
 header = dashboardHeader(
@@ -77,6 +94,18 @@ sidebar <- dashboardSidebar(
   sidebarMenu(
     id = 'menu_tabs'
     , menuItem('Annual Price Change vs Standard Deviation of Daily Change Categories', tabName = 'APCvsSTDDCC')
+  ),
+  sidebarMenu(
+    id = 'menu_tabs'
+    , menuItem('Distibution of Funds Valuation For Contribution', tabName = 'DoFVfC')
+  ),
+  sidebarMenu(
+    id = 'menu_tabs'
+    , menuItem('Distibution of Number of People in Funds For Fund Types', tabName = 'DoNoPiFfFT') #Distibution of Number of People in Funds For Fund Types
+  ),
+  sidebarMenu(
+    id = 'menu_tabs'
+    , menuItem('Most Valued Funds', tabName = 'mvf') #Most Valued Funds
   ),
   sidebarMenu(
     id = 'menu_tabs'
@@ -120,6 +149,18 @@ body <- dashboardBody(
       )
     ),
     tabItem(
+      tabName = 'DoFVfC', #Distibution of Funds Valuation For Contribution
+      plotOutput('DoFVfCPlot')
+    ),
+    tabItem(
+      tabName = 'DoNoPiFfFT', #Distibution of Number of People in Funds For Fund Types
+      plotOutput('DoNoPiFfFTPlot')
+    ),
+    tabItem(
+      tabName = 'mvf', #Most Valued Funds
+      DT::DTOutput('mvfTable')
+    ),
+    tabItem(
       tabName = 'miSources',
       paste('test')
     )
@@ -156,7 +197,7 @@ server <- function(input, output, session) {
   })
   output$APCvsSTDDCCPlot = renderPlot({
     ggplot(df_plot_categories %>%
-             filter(category %in% input$categoryAPCvsSTDDCCInput)
+             dplyr::filter(category %in% input$categoryAPCvsSTDDCCInput)
     )+
       geom_point(aes(x=annual_change, y=stdev, color=category))+
       scale_x_log10()+
@@ -166,6 +207,22 @@ server <- function(input, output, session) {
       facet_wrap(vars(fund_type))+
       labs(title = 'Annual Price Change vs Daily Change Std for Categories', x='Annual Price Change', y='Daily Change Std') 
   })
+  output$DoFVfCPlot = renderPlot({
+    ggplot(df_today)+
+      geom_histogram(bins=50,aes(x=total_value_millions))+
+      facet_grid(rows=vars(contribution)) +
+      labs( x='Total Value in Million', y='Count') 
+  })
+  output$DoNoPiFfFTPlot = renderPlot({
+    ggplot(df_today_investors) +
+      geom_histogram(bins=50,aes(x=people))+facet_wrap(vars(fund_type)) +
+      labs( x='People', y='Count')
+  })
+  output$mvfTable = DT::renderDT(
+    df_valued_funds %>%
+      arrange(desc(change_price_percentage)) %>%
+      select(fund_type, category, code, name, previous_price, price, change_price_percentage)
+  )
 }
 
 # Run the application 
